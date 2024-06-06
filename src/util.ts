c3a1a82e08410986a67263abc83c6a9e2a621a3b
Jose Robles <jd.robles@gmail.com>
2024-06-06 18:35:04 +0200

    Hashing with filename too

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 const genHashFromFile = (fileName: string): string => {
   const fileBuffer = fs.readFileSync(fileName);
   const hash = crypto.createHash("sha256");
+  hash.update(fileName);
   hash.update(fileBuffer);
 
   return hash.digest("hex");
